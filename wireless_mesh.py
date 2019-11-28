e783f1053d66270d2d05130030498e5899365393
hisham8383 <hisham83835@gmail.com>
2019-11-28 19:36:44 +0300

    added mobility

--- wireless_mesh.py
+++ wireless_mesh.py
@@ -24,17 +24,16 @@ def topology(mobility):
     net = Mininet_wifi(link=wmediumd, wmediumd_mode=interference, controller=Controller)
 
     info("*** Creating nodes\n")
-    if mobility:
-        host = net.addHost('host')
-        sta1 = net.addStation('sta1', position='20,70,0', range=5)
-        sta2 = net.addStation('sta2', position='50,70,0', range=5)
-        sta3 = net.addStation('sta3', position='80,70,0', range=5)
-        ap1 = net.addAccessPoint('ap1', wlans=2, ssid='ssid1,ssid12', position='35,60,0', range='30,30')
-        ap2 = net.addAccessPoint('ap2', wlans=2, ssid='ssid2,ssid22', position='65,60,0', range='30,30')
-        ap3 = net.addAccessPoint('ap3', wlans=2, ssid='ssid1,ssid12', position='25,30,0', range='20,20')
-        ap4 = net.addAccessPoint('ap4', wlans=2, ssid='ssid2,ssid22', position='50,30,0', range='20,20')
-        ap5 = net.addAccessPoint('ap5', wlans=2, ssid='ssid1,ssid12', position='75,30,0', range='20,20')
-        c0 = net.addController('c0')
+    host = net.addHost('host')
+    sta1 = net.addStation('sta1', range=5)
+    sta2 = net.addStation('sta2', range=5)
+    sta3 = net.addStation('sta3', range=5)
+    ap1 = net.addAccessPoint('ap1', wlans=2, ssid='ssid1,ssid12', position='35,60,0', range='30,30')
+    ap2 = net.addAccessPoint('ap2', wlans=2, ssid='ssid2,ssid22', position='65,60,0', range='30,30')
+    ap3 = net.addAccessPoint('ap3', wlans=2, ssid='ssid1,ssid12', position='25,30,0', range='20,20')
+    ap4 = net.addAccessPoint('ap4', wlans=2, ssid='ssid2,ssid22', position='50,30,0', range='20,20')
+    ap5 = net.addAccessPoint('ap5', wlans=2, ssid='ssid1,ssid12', position='75,30,0', range='20,20')
+    c0 = net.addController('c0')
 
     info("*** Configuring Propagation Model\n")
     net.setPropagationModel(model="logDistance", exp=4)
@@ -46,13 +45,9 @@ def topology(mobility):
     net.addLink(ap1, host, bw=10, delay='30ms')
 
     net.plotGraph(max_x=100, max_y=100)
-
-    # if mobility:
-    #     net.plotGraph(max_x=100, max_y=100)
-    #     net.startMobility(time=0, model='RandomDirection',
-    #                       max_x=100, max_y=100,
-    #                       min_v=0.5, max_v=0.8, seed=20)
-
+    net.startMobility(time=0, model='RandomDirection', min_x=5,
+                          max_x=90, min_y=10, max_y=90,
+                          min_v=0.5, max_v=0.8, seed=20)
     info("*** Starting network\n")
     net.build()
     c0.start()
@@ -71,6 +66,6 @@ def topology(mobility):
 
 if __name__ == '__main__':
     setLogLevel('info')
-    mobility = True if '-m' in sys.argv else False
+    mobility = True
     topology(mobility)
 
